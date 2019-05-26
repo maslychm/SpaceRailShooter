156f8f80d5d922b492815b68c8f6b501e46f27d9
maslychm <43874961+maslychm@users.noreply.github.com>
2019-05-25 23:51:34 -0400

    addeship movement on screen

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 30f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 30f;
+    [Tooltip("In m")] [SerializeField] float xRange = 13f;
+    [Tooltip("In m")] [SerializeField] float yRange = 8f;
+
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        float newXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange);
+        float newYPos = Mathf.Clamp(transform.localPosition.y + yOffset, -yRange, yRange);
+
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+    }
+}

77b9d07891d0c64562ead2dca06c41feef749a1d
maslychm <43874961+maslychm@users.noreply.github.com>
2019-06-01 17:44:45 -0400

    Added collisions, deth sequence

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -6,6 +6,15 @@ public class MusicPlayer : MonoBehaviour
 {
     private void Awake()
     {
-        DontDestroyOnLoad(this);
-    }
+        int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+
+        if (numMusicPlayers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this);
+        }
+     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] float controlSpeed = 50f;
+    [Tooltip("In m")] [SerializeField] float xRange = 20f;
+    [Tooltip("In m")] [SerializeField] float yRange = 15f;
+
+    [Header("Screen-position Based")]
+    [SerializeField] float positionYawFactor = 2.15f;
+    [SerializeField] float positionPitchFactor = -2.15f;
+
+    [Header("Control-throw Based")]
+    [SerializeField] float controlRollFactor = -30f;
+    [SerializeField] float controlPitchFactor = -25f;
+
+    float xThrow, yThrow;
+    bool isControlEnabled = true;
+
+    void Update()
+    {
+        if (isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        } 
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * controlSpeed * Time.deltaTime;
+        float yOffset = yThrow * controlSpeed * Time.deltaTime;
+
+        float newXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange);
+        float newYPos = Mathf.Clamp(transform.localPosition.y + yOffset, -yRange, yRange);
+
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+    }
+
+    // Called by string reference
+    void OnPlayerDeath()
+    {
+        isControlEnabled = false;
+    }
+}

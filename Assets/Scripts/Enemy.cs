cb67e3733d440fd54008002ff21ff8083db3bbb1
maslychm <43874961+maslychm@users.noreply.github.com>
2019-06-01 18:07:13 -0400

    particle collisions, destroy enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void Start()
+    {
+        Collider enemyCollider = gameObject.AddComponent<BoxCollider>();
+        enemyCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+    }
+}
